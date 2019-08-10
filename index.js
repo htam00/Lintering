a816063c824afa4bbf83c8873af8e7c5c6d4eee2
htam <anonimozurik@gmail.com>
2019-08-10 17:09:08 -0300

    iniciando os comentarios

---
+++ index.js
@@ -0,0 +1,13 @@
+// Declarando a função sendMessage()
+function sendMessage(){
+    // Variavel
+    const Message = "Hello Like Logs" 
+
+    // loops 
+    for(let i = 0; i < 5; i++){
+        console.log(Message) // mostrar 5 vezes a mensagem
+    }
+}
+
+// Chamando a Função sendMessage("saidaDeMessage(PT_BR)")
+sendMessage()
