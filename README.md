78f21a2ac7ab90a1143f4437bfd9021243ef7d7e
htam <anonimozurik@gmail.com>
2019-08-10 17:52:01 -0300

    updated

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
 # Lintering
 
 
+### instalando o modulo eslint 
+###### -D || --save-dev 
+> npm i -D eslint
+
 ### iniciando o eslint no seu app
 > ./node_modules/.bin/eslint --init
 
-### se error na linha console.log escreva isso
+### se error na linha console.log >> escreva este comentario acima do code
 > // eslint-disable-next-line no-console
--- index.js
+++ index.js
@@ -1,11 +1,23 @@
+// © Matheus Luna
+// https://matheusluna.dev/blog
+// Clean Code, Code Professional
+// ESLint
+
+// Declarando variavel global
+let item
+
+// escrenvendo um valor Type:String
+// na variavel item
+item = ["chapeu", "camisa", "sticker"]
+
 // Declarando a função sendMessage()
 function sendMessage(){
-    // Variavel
+    // Variavel local
     const Message = "Hello Like Logs" 
 
     // loops 
     for(let i = 0; i < 5; i++){
-        console.log(Message) // mostrar 5 vezes a mensagem
+        console.log(`${i}: ${item[i]}`) // mostrar 5 vezes a mensagem
     }
 }
 
